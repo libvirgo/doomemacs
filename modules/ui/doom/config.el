c473e30cba6d3d9cfd8aecd2fa3434efadc1106d
libvirgo <sakurapetgirl@live.com>
2024-07-29 20:51:57 +0800

    feat!(ui): add +auto feature for change theme by system or time.

--- modules/ui/doom/config.el
+++ modules/ui/doom/config.el
@@ -8,7 +8,20 @@
 (use-package! doom-themes
   ;; improve integration w/ org-mode
   :hook (doom-load-theme . doom-themes-org-config)
-  :init (setq doom-theme 'doom-one)
+  :init 
+  (when (and (modulep! :os macos) (modulep! +auto))
+    (defun my/load-theme (appearance)
+      "Load theme, taking current system APPEARANCE into consideration."
+      (mapc #'disable-theme custom-enabled-themes)
+      (pcase appearance
+        ('light (load-theme doom-theme-light t))
+        ('dark (load-theme doom-theme-dark t))))
+    (add-hook 'ns-system-appearance-change-functions #'my/load-theme))
+  (when (not (modulep! +auto))
+    (if doom-theme-light
+        (load-theme doom-theme-light t)
+      (load-theme doom-theme-dark t)))
+  ;; (setq doom-theme 'doom-one)
   ;; more Atom-esque file icons for neotree/treemacs
   ;; (when (modulep! :ui neotree)
   ;;   (add-hook 'doom-load-theme-hook #'doom-themes-neotree-config)
@@ -17,6 +30,13 @@
   ;;         doom-themes-neotree-line-spacing 2))
   )
 
+(use-package! circadian
+  :when (and (not (modulep! :os macos)) (modulep! +auto))
+  :hook (doom-after-init . circadian-setup)
+  :config
+  (setq circadian-themes `((:sunrise . ,doom-theme-light)
+                           (:sunset . ,doom-theme-dark)))
+  )
 
 (use-package! solaire-mode
   :hook (doom-load-theme . solaire-global-mode)
--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -3,3 +3,6 @@
 
 (package! doom-themes :pin "2c794a09b023bda09d2f36a7be80684f4e416d88")
 (package! solaire-mode :pin "c9334666bd208f3322e6118d30eba1b2438e2bb9")
+(when (and (not (modulep! :os macos)) (modulep! +auto))
+  (package! circadian :pin "76464419f69e9758bc5a76b2420c9648ddf93dba")
+  )
