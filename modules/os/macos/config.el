5fa988f643de1b248344e079c741885a781c033b
libvirgo <sakurapetgirl@live.com>
2024-07-29 20:50:44 +0800

    feat!(os): mac os add jp keyboard translator and smart disable touchpad change font scale.

--- modules/os/macos/config.el
+++ modules/os/macos/config.el
@@ -7,7 +7,7 @@
 ;; variants thereof), since it requires no additional setup.
 (setq locate-command "mdfind")
 
-
+(setq dired-use-ls-dired nil)
 ;;
 ;;; Compatibilty fixes
 
@@ -38,19 +38,56 @@
 (setq delete-by-moving-to-trash (not noninteractive))
 
 
-;;
-;;; Packages
+;; translate mac keyboard yen to backslash
+(defcustom doom-keyboard-type nil
+  "if doom-keyboard-type is jp, while translate mac keyboard yen to backslash"
+  :group 'doom
+  :type 'symbol)
 
-(use-package! osx-trash
-  ;; DEPRECATED: Not needed on Emacs 29+. Remove when dropping 28 support.
-  ;;   Fixed by https://debbugs.gnu.org/cgi/bugreport.cgi?bug=21340.
-  :when (< emacs-major-version 29)
-  :commands osx-trash-move-file-to-trash
-  :init
-  ;; Lazy load `osx-trash'
-  (when (not (fboundp 'system-move-file-to-trash))
-    (defun system-move-file-to-trash (file)
-      "Move FILE to trash."
-      (when (and (not (featurep :system 'linux))
-                 (not (file-remote-p default-directory)))
-        (osx-trash-move-file-to-trash file)))))
+(defun mac-translate-from-yen-to-backslash ()
+  ;; Convert yen to backslash for JIS keyboard.
+  (interactive)
+  (when (eq doom-keyboard-type 'jp)
+    (define-key global-map [165] nil)
+    (define-key global-map [2213] nil)
+    (define-key global-map [3420] nil)
+    (define-key global-map [67109029] nil)
+    (define-key global-map [67111077] nil)
+    (define-key global-map [8388773] nil)
+    (define-key global-map [134219941] nil)
+    (define-key global-map [75497596] nil)
+    (define-key global-map [201328805] nil)
+    (define-key function-key-map [165] [?\\])
+    (define-key function-key-map [2213] [?\\]) ;; for Intel
+    (define-key function-key-map [3420] [?\\]) ;; for PowerPC
+    (define-key function-key-map [67109029] [?\C-\\])
+    (define-key function-key-map [67111077] [?\C-\\])
+    (define-key function-key-map [8388773] [?\M-\\])
+    (define-key function-key-map [134219941] [?\M-\\])
+    (define-key function-key-map [75497596] [?\C-\M-\\])
+    (define-key function-key-map [201328805] [?\C-\M-\\])))
+
+(add-hook! 'doom-first-buffer-hook #'mac-translate-from-yen-to-backslash)
+
+(defvar using-trackpad-timer nil)
+(defvar using-trackpad nil)
+
+(defun mouse-present-p ()
+  (with-temp-buffer
+    (call-process "ioreg" nil (current-buffer) nil "-p" "IOUSB")
+    (goto-char (point-min))
+    (and (search-forward "USB Receiver" nil t) t)))
+
+(defun set-using-trackpad ()
+  (setq using-trackpad (not (mouse-present-p))))
+
+(defun maybe-mouse-wheel-text-scale (event)
+  (interactive (list last-input-event))
+  (when (not using-trackpad)
+    (mouse-wheel-text-scale event)))
+
+(when using-trackpad-timer
+  (cancel-timer using-trackpad-timer))
+(setq using-trackpad-timer (run-at-time "0" 60 'set-using-trackpad))
+(global-set-key [(control wheel-up)] 'maybe-mouse-wheel-text-scale)
+(global-set-key [(control wheel-down)] 'maybe-mouse-wheel-text-scale)
