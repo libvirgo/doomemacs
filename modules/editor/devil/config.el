13b55bc5be00622a7beee430fd912d9c994778ed
libvirgo <sakurapetgirl@live.com>
2024-08-14 04:01:39 +0800

    feat(editor/devil): add devil mode.
    Summary:
    save pinky.
    Test Plan:

---
+++ modules/editor/devil/config.el
@@ -0,0 +1,5 @@
+;;; editor/devil/config.el -*- lexical-binding: t; -*-
+
+(use-package! devil
+  :init
+  (global-devil-mode))
---
+++ modules/editor/devil/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; editor/devil/packages.el
+
+(package! devil :pin "c2dc9fc2886471daff654e12b46a144a4d1361ae")
