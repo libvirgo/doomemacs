c3a508c2e1c6b241d946a0a6a5eac6fde0577a3c
libvirgo <sakurapetgirl@live.com>
2024-08-14 03:59:01 +0800

    feat(ui): add ef themes.
    Summary:
    
    Test Plan:

--- modules/ui/doom/config.el
+++ modules/ui/doom/config.el
@@ -6,10 +6,19 @@
 
 
 (use-package! doom-themes
+  :unless (modulep! +ef-themes)
   ;; improve integration w/ org-mode
   :hook (doom-load-theme . doom-themes-org-config)
-  :init 
-  (when (and (modulep! :os macos) (modulep! +auto))
+  ;; (setq doom-theme 'doom-one)
+  ;; more Atom-esque file icons for neotree/treemacs
+  ;; (when (modulep! :ui neotree)
+  ;;   (add-hook 'doom-load-theme-hook #'doom-themes-neotree-config)
+  ;;   (setq doom-themes-neotree-enable-variable-pitch t
+  ;;         doom-themes-neotree-file-icons 'simple
+  ;;         doom-themes-neotree-line-spacing 2))
+  )
+
+(when (and (modulep! :os macos) (modulep! +auto))
     (defun my/load-theme (appearance)
       "Load theme, taking current system APPEARANCE into consideration."
       (mapc #'disable-theme custom-enabled-themes)
@@ -21,14 +30,6 @@
     (if doom-theme-light
         (load-theme doom-theme-light t)
       (load-theme doom-theme-dark t)))
-  ;; (setq doom-theme 'doom-one)
-  ;; more Atom-esque file icons for neotree/treemacs
-  ;; (when (modulep! :ui neotree)
-  ;;   (add-hook 'doom-load-theme-hook #'doom-themes-neotree-config)
-  ;;   (setq doom-themes-neotree-enable-variable-pitch t
-  ;;         doom-themes-neotree-file-icons 'simple
-  ;;         doom-themes-neotree-line-spacing 2))
-  )
 
 (use-package! circadian
   :when (and (not (modulep! :os macos)) (modulep! +auto))
--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,6 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
-
+(when (modulep! +ef-themes)
+  (package! ef-themes :pin "5e902f286bc052bcc200826d0677bd17f8f21b12")
+  )
 (package! doom-themes :pin "2c794a09b023bda09d2f36a7be80684f4e416d88")
 (package! solaire-mode :pin "c9334666bd208f3322e6118d30eba1b2438e2bb9")
 (when (and (not (modulep! :os macos)) (modulep! +auto))
