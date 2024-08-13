201303123203649681e5526b0f45d4e1eb8ea985
Henrik Lissner <git@henrik.io>
2024-08-14 04:08:26 +0800

    bump: :ui
    Alexander-Miller/treemacs@54ef590b7621 -> Alexander-Miller/treemacs@202d7f01d613
    Bad-ptr/persp-mode.el@345baaa520ab -> Bad-ptr/persp-mode.el@40e9993a9711
    dgutov/diff-hl@57d9d4e3e173 -> dgutov/diff-hl@b80ff9b4a772
    ema2159/centaur-tabs@d6009c295a43 -> ema2159/centaur-tabs@063534bd0038
    emacs-lsp/lsp-treemacs@1d43e9e0307f -> emacs-lsp/lsp-treemacs@fb1a07ae0a3d
    emacs-straight/minimap@4898d277686c -> emacs-straight/minimap@5aa0df40bc0d
    seagle0128/doom-modeline@1505c13564b8 -> seagle0128/doom-modeline@9920ef511620
    tarsius/hl-todo@dd46d426c050 -> tarsius/hl-todo@82eba6b8f7b5

--- modules/ui/modeline/packages.el
+++ modules/ui/modeline/packages.el
@@ -1,8 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/modeline/packages.el
 
+<<<<<<< dest:   25da737c3d03 - git: feat(lib): doom-org-docs-mode: bind q to ...
 (unless (or (modulep! +light) (modulep! +simple))
-  (package! doom-modeline :pin "1505c13564b83e44d3187611e326a48b742cad3a"))
+  (package! doom-modeline :pin "9920ef511620e9fa5599cb357e48487f758b1bb1"))
 (when (modulep! +simple)
   (package! awesome-tray :recipe (:host github :repo "manateelazycat/awesome-tray")))
 (package! anzu :pin "26fb50b429ee968eb944b0615dd0aed1dd66172c")
